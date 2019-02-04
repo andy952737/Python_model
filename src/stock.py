4e91ef27148fa730d1d73585d459bf708fd6b506
Lian-an Kuan <andy952737@gmail.com>
2019-02-04 10:09:48 +0800

    2/4

--- src/stock.py
+++ src/stock.py
@@ -3,17 +3,17 @@ import pandas as pd
 
 site = "https://query1.finance.yahoo.com/v7/finance/download/2330.TW?period1=0&period2=1549258857&interval=1d&events=history&crumb=hP2rOschxO0"
 response = requests.post(site)
-# print(response.text)
+# print(response.text) 
 
-with open('file.csv', 'w') as f:
+with open('data/stock.csv', 'w') as f:
  f.writelines(response.text)
- 
+  
 #before
-# df = pd.read_csv('file.csv')
+# df = pd.read_csv('data/stock.csv')
 # print(df.head())
 
 #after
-df = pd.read_csv('file.csv', index_col='Date', parse_dates=['Date'])
+df = pd.read_csv('data/stock.csv', index_col='Date', parse_dates=['Date'])
 print(df.head())
 
 df.Close.plot()

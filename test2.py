c1b1f5d6270ff02fce4492e44845f32b2d1d7d8c
Andy <andy952737@gmail.com>
2019-07-10 14:41:28 +0800

    7/10

--- test2.py
+++ test2.py
@@ -1,1 +1,10 @@
-import tenorflow as tf
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+# 文件名：test2.py
+# Python檔案處理篇
+# https://ithelp.ithome.com.tw/articles/10161708
+
+f = open('data/test.txt','r')
+print(f.read())
+
+#f.write('IT人')

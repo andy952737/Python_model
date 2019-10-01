74ee77feb71cce6ea1100722e3eb488d6513151a
Andy <andy952737@gmail.com>
2019-10-01 14:47:54 +0800

    10/1

--- GPIO/gpio_test.py
+++ GPIO/gpio_test.py
@@ -1,11 +1,9 @@
 #!/usr/bin/python
 # -*- coding: UTF-8 -*-
 # 文件名：test.py
-# Python GPIO 應用篇
+# Python GPIO 樹莓派 應用篇
 # http://andychen15577.blogspot.com/2016/08/raspberry-pi3-python-raspberry-pi-3-gpio.html
+# https://sites.google.com/site/rasberrypintust/raspberry-pi-shu-mei-pai-shi-qian-zhun-bei/python-gipo-an-zhuang
 
 import RPi.GPIO as GPIO
-
-
-
-
+print  (GPIO.VERSION)
